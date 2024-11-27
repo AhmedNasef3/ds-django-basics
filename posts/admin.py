eed7246d3b6c22a20e8172294bb467ded644f3de
Ahmed Wael <ahmednasef9889@gmail.com>
2024-11-27 19:48:43 +0200

    save project stage-1

---
+++ posts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post,Author
+from django_summernote.admin import SummernoteModelAdmin
+# Register your models here.
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+    list_display=['title','author']
+    list_filter=['tags','author']
+    search_fields=['title','content']
+
+# my own class 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=['name','email']
+    search_fields=['name','email']
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Author,AuthorAdmin)
